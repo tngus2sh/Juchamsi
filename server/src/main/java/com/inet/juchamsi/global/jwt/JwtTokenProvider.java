ceb704ceaae261008c9120736e87b42e06b542c8
tngus2sh <ssuhyeon98@gmail.com>
2023-07-23 23:51:57 +0900

    feat: #S09P12C107-88 JWT 토큰 설정 - JwtTokenProvider : 토큰 기능 구현 - SecurityUtil : security 설정 - TokenInfo : 토큰 정보 DTO 생성 - build.gradle : JWT 토큰을 위한 jsonwebtoken 의존성 추가

--- server/src/main/java/com/inet/juchamsi/global/jwt/JwtTokenProvider.java
+++ server/src/main/java/com/inet/juchamsi/global/jwt/JwtTokenProvider.java
@@ -1,4 +1,113 @@
 package com.inet.juchamsi.global.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
 public class JwtTokenProvider {
+
+    // JWT 서명에 사용되는 Key 객체
+    private final Key key;
+
+    // JWT의 시크릿 키를 받아서 해당 키를 사용하여 토큰을 생성 및 유효성을 검증하는 JwtTokenProvider 클래스의 초기화 수행
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
+        // JWT의 시크릿 키는 일반적으로 BASE64 인코딩된 형태로 저장되어 있음 -> 디코팅을 통해 원래의 바이트 배열 형태로 변환
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        // keyBytes를 기반으로 JWT서명을 생성하는데 사용되는 Key 객체를 생성
+        // key 객체는 JwtTokenProvider 클래스내의 다른 메서들에서 JWT 서명 생성 및 검증에 사용됨
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // 유저 정보를 가지고 AccessToken, RefreshToken을 생성하는 메소드
+    public TokenInfo generateToken(Authentication authentication) {
+        // 권한 정보를 가져오기 위해 Authentication 객체로부터 사용자의 권한(authorities)들을 가져온다
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        // Access Token 생성
+        Date accessTokenExpriesIn = new Date(now + 86_400_000); // 현재시간을 기준으로 24시간(86,400,000밀리초) 뒤 토큰 만료
+        String accessToekn = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities)
+                .setExpiration(accessTokenExpriesIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        // Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + 86_400_000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return TokenInfo.builder()
+                .grantType("Bearer")
+                .accessToken(accessToekn)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메소드
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get("auth").toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 반환
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    // 토큰 정보를 검증하는 메소드
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJwt(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("UnSupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty", e);
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJwt(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
 }
