b38fb3f2c8f1e86d71faa5416b4a876e08fecc86
이진하 <ljh8190@naver.com>
2023-07-24 16:03:27 +0900

    feat: #S09P12C107-150 swagger를 이용해 villa controller 스켈레톤 코드 작성

--- server/src/main/java/com/inet/juchamsi/domain/villa/api/VillaApiController.java
+++ server/src/main/java/com/inet/juchamsi/domain/villa/api/VillaApiController.java
@@ -1,11 +1,13 @@
 package com.inet.juchamsi.domain.villa.api;
 
 import com.inet.juchamsi.domain.villa.api.request.CreateVillaRequest;
+import com.inet.juchamsi.domain.villa.api.request.ModifyVillaRequest;
 import com.inet.juchamsi.domain.villa.api.response.VillaResponse;
 import com.inet.juchamsi.domain.villa.application.VillaService;
 import com.inet.juchamsi.global.api.ApiResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,21 +22,28 @@ public class VillaApiController {
 
     private final VillaService villaService;
 
+
     @ApiOperation(value = "빌라 등록", notes = "새로운 빌라를 등록합니다")
-    @PostMapping("/")
-    public ApiResult<Void> createVilla(@RequestBody CreateVillaRequest request) {
+    @PostMapping("")
+    public ApiResult<Void> createVilla(@ApiParam(value = "villa-dto") @RequestBody CreateVillaRequest request) {
         return null;
     }
 
     @ApiOperation(value = "빌라 상세 조회", notes = "사용자는 자신의 빌라 상세 정보를 조회합니다")
     @GetMapping("/{villa_id}")
-    public ApiResult<VillaResponse> showVillaDetail(@PathVariable("villa_id") Long villaId) {
+    public ApiResult<VillaResponse> showVillaDetail(@ApiParam(value = "villa-id") @PathVariable("villa_id") Long villaId) {
+        return null;
+    }
+
+    @ApiOperation(value = "빌라 수정", notes = "등록된 빌라 정보를 수정합니다")
+    @PutMapping("")
+    public ApiResult<Void> modifyVilla(@ApiParam(value = "villa-dto") @RequestBody ModifyVillaRequest request) {
         return null;
     }
 
     @ApiOperation(value = "빌라 삭제", notes = "등록된 빌라를 삭제합니다")
-    @DeleteMapping("{villa_id}")
-    public ApiResult<Void> removeViila(@PathVariable("villa_id") Long villaId) {
+    @DeleteMapping("/{villa_id}")
+    public ApiResult<Void> removeViila(@ApiParam(value = "villa-id") @PathVariable("villa_id") Long villaId) {
         return null;
     }
 }
