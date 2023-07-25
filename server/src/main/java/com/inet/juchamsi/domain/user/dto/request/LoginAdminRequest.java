e3e5f7fbf25e34cb0dd1a043ad0ef31c1b019f91
소수현 <ssuhyeon98@gmail.com>
2023-07-25 18:03:05 +0900

    feat: LoginAdminRequest 생성 S09P12C107-185

--- server/src/main/java/com/inet/juchamsi/domain/user/dto/request/LoginAdminRequest.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/dto/request/LoginAdminRequest.java
@@ -1,4 +1,11 @@
 package com.inet.juchamsi.domain.user.dto.request;
 
+import lombok.Data;
+
+@Data
 public class LoginAdminRequest {
+    
+    private String loginId;
+    private String password;
+    
 }
