85f343a5507feb1b40ef1d4027ce4007202864ef
JH <ljh8190@naver.com>
2023-07-25 21:39:20 +0900

    refactor: 사용하지 않는 import문 제거

--- server/src/main/java/com/inet/juchamsi/domain/villa/application/impl/VillaServiceImpl.java
+++ server/src/main/java/com/inet/juchamsi/domain/villa/application/impl/VillaServiceImpl.java
@@ -94,5 +94,4 @@ public class VillaServiceImpl implements VillaService {
         villa.setActive(DISABLED);
     }
 
-
 }
--- server/src/main/java/com/inet/juchamsi/domain/villa/entity/Villa.java
+++ server/src/main/java/com/inet/juchamsi/domain/villa/entity/Villa.java
@@ -5,7 +5,6 @@ import com.inet.juchamsi.global.common.TimeBaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
 
