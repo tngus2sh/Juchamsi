6dfdccff0066699b6081ca882178a7301baf2d2f
tngus2sh <ssuhyeon98@gmail.com>
2023-07-25 23:41:15 +0900

    refactor: 회원 상세 조회 service 구현 #S09P12C107-199

--- server/src/main/java/com/inet/juchamsi/domain/user/application/AdminService.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/application/AdminService.java
@@ -1,10 +1,14 @@
 package com.inet.juchamsi.domain.user.application;
 
 import com.inet.juchamsi.domain.user.dto.request.LoginAdminRequest;
+import com.inet.juchamsi.domain.user.dto.response.AdminResponse;
 
 import javax.transaction.Transactional;
 
 @Transactional
-public interface UserService {
+public interface AdminService {
     Long signup(LoginAdminRequest dto);
+
+    // 회원 상세 조회
+    AdminResponse showDetailUser(String loginId);
 }
---
+++ server/src/main/java/com/inet/juchamsi/domain/user/application/impl/AdminServiceImpl.java
@@ -0,0 +1,46 @@
+package com.inet.juchamsi.domain.user.application.impl;
+
+import com.inet.juchamsi.domain.user.application.AdminService;
+import com.inet.juchamsi.domain.user.dao.UserRepository;
+import com.inet.juchamsi.domain.user.dto.request.LoginAdminRequest;
+import com.inet.juchamsi.domain.user.dto.response.AdminResponse;
+import com.inet.juchamsi.domain.user.entity.User;
+import com.inet.juchamsi.global.error.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {
+    
+    private final UserRepository userRepository;
+    
+    @Override
+    public Long signup(LoginAdminRequest dto) {
+        Optional<Long> phoneNumber = userRepository.existPhoneNumber(dto.getLoginId());
+        
+        return null;
+    }
+
+    // 회원 상세 조회
+    @Override
+    public AdminResponse showDetailUser(String loginId) {
+        // adminLoginId로 회원 상세 정보 가져오기
+        Optional<User> targetUser = userRepository.findByLoginId(loginId);
+
+        if (!targetUser.isPresent()) {
+            throw new NotFoundException(User.class, loginId);
+        }
+
+        User user = targetUser.get();
+        return AdminResponse.builder()
+                .phoneNumber(user.getPhoneNumber())
+                .name(user.getName())
+                .carNumber(user.getCarNumber())
+                .villaNumber(user.getVillaNumber())
+                .build();
+
+    }
+}
