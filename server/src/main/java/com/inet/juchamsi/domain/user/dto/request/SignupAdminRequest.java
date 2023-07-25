ab47d23b8a8f6bb9c384d906abcafdd5ee4360e5
소수현 <ssuhyeon98@gmail.com>
2023-07-25 18:03:19 +0900

    feat: SignupAdminRequest 생성 S09P12C107-185

--- server/src/main/java/com/inet/juchamsi/domain/user/dto/request/SignupAdminRequest.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/dto/request/SignupAdminRequest.java
@@ -1,5 +1,25 @@
 package com.inet.juchamsi.domain.user.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
 public class SignupAdminRequest {
+    
+    @NotBlank
+    @Pattern(regexp = "^[0-9]*$")
+    @ApiModelProperty(example = "12")
+    private Long villaId;
+    private String phoneNumber;
+    private String loginId;
+    private String passsword;
+    private String name;
+    private String grade;
+    private String carNumber;
+    private int villaNumber;
     
 }
