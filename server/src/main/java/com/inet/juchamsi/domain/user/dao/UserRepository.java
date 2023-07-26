e89d133ee4dad54ecd63cb7d95479b2d74568a7a
tngus2sh <ssuhyeon98@gmail.com>
2023-07-27 07:07:15 +0900

    feat: 승인여부, 탈퇴여부 수정 sql 구현 S09P12C107-194

--- server/src/main/java/com/inet/juchamsi/domain/user/dao/UserRepository.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/dao/UserRepository.java
@@ -27,6 +27,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Modifying
     @Query("update User u set u.refreshToken=:refreshToken where u.loginId=:loginId")
-    void updateRefreshToken(@Param("loginId") String loginId, @Param("refreshToken") String refreshToken);
-    
+    Optional<Long> updateRefreshToken(@Param("loginId") String loginId, @Param("refreshToken") String refreshToken);
+
+    @Modifying
+    @Query("update User u set u.approve=:approve where u.loginId=:loginId")
+    Optional<Long> updateApprove(@Param("loginId") String loginId, @Param("approve") String approve);
+
+    @Modifying
+    @Query("update User u set u.active=:active where u.loginId=:loginId")
+    Optional<Long> updateActive(@Param("loginId") String loginId, @Param("active") String active);
 }
