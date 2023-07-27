b52efdc9015be47ea8395089ae8ab0167ff5a7dd
tngus2sh <ssuhyeon98@gmail.com>
2023-07-28 05:47:18 +0900

    feat: 관리자 service 테스트 - 로그인 성공 - 로그인 실패 #S09P12C107-194

--- server/src/test/java/com/inet/juchamsi/user/service/AdminServiceTest.java
+++ server/src/test/java/com/inet/juchamsi/user/service/AdminServiceTest.java
@@ -3,21 +3,30 @@ package com.inet.juchamsi.user.service;
 import com.inet.juchamsi.domain.user.application.AdminService;
 import com.inet.juchamsi.domain.user.dao.UserRepository;
 import com.inet.juchamsi.domain.user.dto.request.CreateOwnerRequest;
+import com.inet.juchamsi.domain.user.dto.request.LoginTenantRequest;
 import com.inet.juchamsi.domain.user.dto.response.AdminResponse;
 import com.inet.juchamsi.domain.user.entity.Approve;
 import com.inet.juchamsi.domain.user.entity.Grade;
 import com.inet.juchamsi.domain.user.entity.User;
+import com.inet.juchamsi.domain.villa.dao.VillaRepository;
+import com.inet.juchamsi.domain.villa.entity.Villa;
 import com.inet.juchamsi.global.common.Active;
 import com.inet.juchamsi.global.error.AlreadyExistException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+
+import static com.inet.juchamsi.global.common.Active.ACTIVE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Transactional
@@ -30,13 +39,17 @@ public class AdminServiceTest {
     UserRepository userRepository;
 
     @Autowired
+    VillaRepository villaRepository;
+
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("회원 상세 조회")
     void showDetailUser() {
         // given
-        User targetUser = insertUser();
+        Villa targetVilla = insertVilla();
+        User targetUser = insertUser(targetVilla);
         String loginId = "adminid";
 
         // when
@@ -52,7 +65,8 @@ public class AdminServiceTest {
     @DisplayName("회원 가입#아이디 중복")
     void createUser() {
         // given
-        User targetUser = insertUser();
+        Villa targetVilla = insertVilla();
+        User targetUser = insertUser(targetVilla);
 
         // when
         CreateOwnerRequest dto = CreateOwnerRequest.builder()
@@ -64,8 +78,41 @@ public class AdminServiceTest {
                 .isInstanceOf(AlreadyExistException.class);
     }
 
-    private User insertUser() {
+
+    @Test
+    @DisplayName("관리자 로그인 ## 로그인 성공")
+    void loginUser() {
+        // given
+        Villa targetVilla = insertVilla();
+        User targetUser = insertUser(targetVilla);
+
+        // when
+        String loginId = "adminid";
+        String password = "userPw123!";
+
+        // then
+        assertNotNull(adminService.login(loginId, password));
+    }
+
+    @Test
+    @DisplayName("관리자 로그인 ## 로그인 실패")
+    void loginUserFail() {
+        // given
+        Villa targetVilla = insertVilla();
+        User targetUser = insertUser(targetVilla);
+
+        // when
+        String loginId = "adminid";
+        String password = "userPw";
+
+        // then
+        Assertions.assertThatThrownBy(() -> adminService.login(loginId, password))
+                .isInstanceOf(BadCredentialsException.class);
+    }
+
+    private User insertUser(Villa villa) {
         User user = User.builder()
+                .villa(villa)
                 .loginId("adminid")
                 .loginPassword(passwordEncoder.encode("userPw123!"))
                 .phoneNumber("01012341234")
@@ -73,7 +120,19 @@ public class AdminServiceTest {
                 .grade(Grade.ADMIN)
                 .approve(Approve.APPROVE)
                 .active(Active.ACTIVE)
+                .roles(Collections.singletonList("ADMIN"))
                 .build();
         return userRepository.save(user);
+    }
+
+    private Villa insertVilla() {
+        Villa villa = Villa.builder()
+                .name("삼성 빌라")
+                .address("광주 광산구 하남산단6번로 107")
+                .idNumber("62218271")
+                .totalCount(6)
+                .active(ACTIVE)
+                .build();
+        return villaRepository.save(villa);
     }
 }
