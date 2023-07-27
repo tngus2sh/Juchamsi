1c0f11d51d40de4a5d008d88e5e4a09fa2aad1f6
소수현 <ssuhyeon98@gmail.com>
2023-07-27 09:53:32 +0900

    refactor: 관리자에서 빌라, 차 관련된 변수 삭제 S09P12C107-194

--- server/src/main/java/com/inet/juchamsi/domain/user/dto/response/AdminResponse.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/dto/response/AdminResponse.java
@@ -2,27 +2,18 @@ package com.inet.juchamsi.domain.user.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.ToString;
 
-@ToString
-@Getter
+@Data
 public class AdminResponse {
 
     private Long id;
-    private Long villaId;
     private String phoneNumber;
     private String name;
-    private String carNumber;
-    private int villaNumber;
 
     @Builder
-    public AdminResponse(Long id, Long villaId, String phoneNumber, String name, String carNumber, int villaNumber) {
+    public AdminResponse(Long id, String phoneNumber, String name) {
         this.id = id;
-        this.villaId = villaId;
         this.phoneNumber = phoneNumber;
         this.name = name;
-        this.carNumber = carNumber;
-        this.villaNumber = villaNumber;
     }
 }
