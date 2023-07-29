e2e7926cc1564b98a255b7af884dafa1b2233014
tngus2sh <ssuhyeon98@gmail.com>
2023-07-29 17:16:47 +0900

    feat: 집주인 api test - 로그아웃 - 회원정보 수정 - 세입자 회원 승인 여부 - 탈퇴 #S09P12C107-197

--- server/src/test/java/com/inet/juchamsi/user/api/OwnerApiTest.java
+++ server/src/test/java/com/inet/juchamsi/user/api/OwnerApiTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inet.juchamsi.domain.user.api.OwnerApiController;
 import com.inet.juchamsi.domain.user.application.OwnerService;
 import com.inet.juchamsi.domain.user.dao.UserRepository;
+import com.inet.juchamsi.domain.user.dto.request.CreateAdminRequest;
 import com.inet.juchamsi.domain.user.dto.request.CreateOwnerRequest;
 import com.inet.juchamsi.domain.user.entity.Approve;
 import com.inet.juchamsi.domain.user.entity.Grade;
@@ -69,14 +70,16 @@ public class OwnerApiTest {
         villaRepository.save(villa);
         userRepository.save(User.builder()
                 .villa(villa)
-                .loginId("ownerid")
+                .loginId("ownerId")
                 .loginPassword(passwordEncoder.encode("userPw123!"))
                 .phoneNumber("01012341234")
                 .name("김주참")
-                .grade(Grade.ADMIN)
+                .grade(Grade.OWNER)
                 .approve(Approve.APPROVE)
                 .active(Active.ACTIVE)
-                .roles(Collections.singletonList("ADMIN"))
+                .carNumber("12가 1234")
+                .villaNumber(201)
+                .roles(Collections.singletonList("OWNER"))
                 .build());
 
     }
@@ -86,7 +89,7 @@ public class OwnerApiTest {
     @DisplayName("집주인 회원가입")
     void showDetailUser() throws Exception {
         // given
-        String loginId = "ownerid";
+        String loginId = "ownerId";
 
         // when
         ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get("/owner/{id}", loginId));
@@ -105,7 +108,7 @@ public class OwnerApiTest {
     void duplicatedUserLoginId() throws Exception {
         // given
         String object = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
-                .loginId("ownerid")
+                .loginId("ownerId")
                 .build());
 
         // when
@@ -124,7 +127,7 @@ public class OwnerApiTest {
     void duplicatedUserPhoneNumber() throws Exception {
         // given
         String object = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
-                .loginId("ownerid")
+                .loginId("ownerId")
                 .build());
 
         // when
@@ -144,7 +147,7 @@ public class OwnerApiTest {
     void loginUser() throws Exception {
         // given
         String object = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
-                .loginId("ownerid")
+                .loginId("ownerId")
                 .loginPassword("userPw123!")
                 .build());
 
@@ -164,7 +167,7 @@ public class OwnerApiTest {
     void loginUserFailInvalidId() throws Exception {
         // given
         String object = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
-                .loginId("ownerid12")
+                .loginId("ownerId212")
                 .loginPassword("userPw123!")
                 .build());
 
@@ -184,7 +187,7 @@ public class OwnerApiTest {
     void loginUserFailWrongPassword() throws Exception {
         // given
         String object = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
-                .loginId("ownerid")
+                .loginId("ownerId")
                 .loginPassword("userPw")
                 .build());
 
@@ -199,5 +202,219 @@ public class OwnerApiTest {
                 .andExpect(jsonPath("$.success").value(false));
     }
 
+    @Test
+    @DisplayName("관리자 로그아웃")
+    void  logoutUser() throws Exception {
+        // given
+        String ownerId = "ownerId";
 
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get("/owner/logout/{id}", ownerId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(true));
+    }
+
+    @Test
+    @DisplayName("집주인  회원정보 수정")
+    void modifyUser() throws Exception {
+        // given
+        String object  = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
+                .villaIdNumber("62218271")
+                .phoneNumber("01099998888")
+                .loginId("ownerId")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .name("김주참")
+                .carNumber("12가 1234")
+                .villaNumber(201)
+                .grade(Grade.OWNER.name())
+                .build());
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/owner")
+                .content(object)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(true));
+    }
+
+    @Test
+    @DisplayName("집주인 회원정보 수정 ## 해당 회원이 없을 때")
+    void modifyUserNoPresent() throws Exception {
+        // given
+        String object  = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
+                .villaIdNumber("62218271")
+                .phoneNumber("01099998888")
+                .loginId("ownerIdssss")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .name("박주참")
+                .carNumber("12가 1234")
+                .villaNumber(201)
+                .grade(Grade.OWNER.name())
+                .build());
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/owner")
+                .content(object)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.message").value("해당 회원을 찾을 수가 없습니다"));
+    }
+
+    @Test
+    @DisplayName("집주인 회원정보 수정 ## 핸드폰 중복")
+    void modifyUserDuplicatedPhoneNumber() throws  Exception {
+        // given
+        compareUser();
+        String object  = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
+                .villaIdNumber("62218271")
+                .phoneNumber("01098765432")
+                .loginId("ownerId")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .name("김주참")
+                .grade(Grade.OWNER.name())
+                .carNumber("12가 1234")
+                .villaNumber(201)
+                .build());
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/owner")
+                .content(object)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.message").value("이미 존재하는 핸드폰 번호입니다."));
+    }
+
+    @Test
+    @DisplayName("집주인 회원정보 수정 ## 해당 빌라 없을 때")
+    void modifyUserNoPresentVilla() throws  Exception {
+        // given
+        String object  = objectMapper.writeValueAsString(CreateOwnerRequest.builder()
+                .villaIdNumber("623-8271")
+                .phoneNumber("01099998888")
+                .loginId("ownerId")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .name("김주참")
+                .grade(Grade.OWNER.name())
+                .carNumber("12가 1234")
+                .villaNumber(201)
+                .build());
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/owner")
+                .content(object)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.message").value("해당 회원을 찾을 수가 없습니다"));
+    }
+
+    @Test
+    @DisplayName("세입자 회원가입 요청 처리")
+    void manageApprove() throws Exception {
+        // given
+        tenantUser();
+        String tenantId = "tenantId";
+        String approve = Approve.APPROVE.name();
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get("/owner/{id}/{approve}", tenantId, approve));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(true));
+    }
+
+    @Test
+    @DisplayName("세입자 회원가입 요청 처리 ## 회원 존재하지 않을 때")
+    void manageApproveNoPresent() throws Exception {
+        // given
+        String tenantId = "tenantId";
+        String approve = Approve.APPROVE.name();
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get("/owner/{id}/{approve}", tenantId, approve));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(false));
+    }
+
+    @Test
+    @DisplayName("관리자 탈퇴")
+    void removeUser() throws Exception {
+        // given
+        String ownerId = "ownerId";
+
+        // when
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.delete("/owner/{id}", ownerId));
+
+        // then
+        actions.andDo(print())
+                .andExpect(jsonPath("$.success").value(true));
+    }
+
+    private void compareUser() {
+        Villa villa = Villa.builder()
+                .name("삼성 빌라")
+                .address("광주 광산구 하남산단6번로 107")
+                .idNumber("62218271")
+                .totalCount(6)
+                .active(ACTIVE)
+                .build();
+        villaRepository.save(villa);
+        User user = User.builder()
+                .villa(villa)
+                .loginId("leeOwner")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .phoneNumber("01098765432")
+                .name("이주참")
+                .grade(Grade.OWNER)
+                .approve(Approve.APPROVE)
+                .active(Active.ACTIVE)
+                .carNumber("98나 1234")
+                .villaNumber(101)
+                .roles(Collections.singletonList("OWNER"))
+                .build();
+        userRepository.save(user);
+    }
+
+    private void tenantUser() {
+        Villa villa = Villa.builder()
+                .name("삼성 빌라")
+                .address("광주 광산구 하남산단6번로 107")
+                .idNumber("62218271")
+                .totalCount(6)
+                .active(ACTIVE)
+                .build();
+        villaRepository.save(villa);
+        userRepository.save(User.builder()
+                .villa(villa)
+                .loginId("tenantId")
+                .loginPassword(passwordEncoder.encode("userPw123!"))
+                .phoneNumber("01099998888")
+                .name("최입자")
+                .grade(Grade.USER)
+                .approve(Approve.WAIT)
+                .active(Active.ACTIVE)
+                .roles(Collections.singletonList("USER"))
+                .build());
+    }
 }
