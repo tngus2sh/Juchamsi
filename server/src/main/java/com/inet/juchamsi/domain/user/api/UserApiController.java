912a9e52e7d7f2b9cb722a419009e768efa23aec
이진하 <ljh8190@naver.com>
2023-07-31 11:53:23 +0900

    feat: S09P12C107-192 아이디 중복 체크 기능 controller 작성

--- server/src/main/java/com/inet/juchamsi/domain/user/api/UserApiController.java
+++ server/src/main/java/com/inet/juchamsi/domain/user/api/UserApiController.java
@@ -7,10 +7,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.inet.juchamsi.global.api.ApiResult.ERROR;
+import static com.inet.juchamsi.global.api.ApiResult.OK;
 
 @RestController
 @Slf4j
@@ -24,7 +28,14 @@ public class UserApiController {
 
     @ApiOperation(value = "아이디 중복 체크", notes = "사용자는 중복되지 않은 아이디로 회원가입 할 수 있다")
     @GetMapping("/id/{id}")
-    public ApiResult<Void> checkId(@ApiParam(value = "user-id") @PathVariable(value = "id") String userId) {
-        return null;
+    public ApiResult<Void> checkId(@ApiParam(value = "user-id") @PathVariable(value = "id") String loginId) {
+        Boolean flag = userService.checkId(loginId);
+
+        if(flag) {
+            return OK(null);
+        }
+        else {
+            return ERROR("동일한 아이디가 존재합니다.", HttpStatus.CONFLICT);
+        }
     }
 }
