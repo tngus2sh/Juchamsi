38ba6c602f448df72e896784f9850390fb6e4ec4
tngus2sh <ssuhyeon98@gmail.com>
2023-07-31 23:38:29 +0900

    feat: 채팅룸 dto #S09P12C107-244

---
+++ server/src/main/java/com/inet/juchamsi/domain/chat/dto/request/ChatMessageDto.java
@@ -0,0 +1,13 @@
+package com.inet.juchamsi.domain.chat.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessageDto {
+
+    private String roomId;
+    private String writer;
+    private String message;
+}
