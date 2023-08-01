6cbd9498d750d87a0825d5bbe3099d6d92e80b27
소수현 <ssuhyeon98@gmail.com>
2023-08-01 17:55:09 +0900

    refactor: 필요없는 import 삭제 S09P12C107-244

--- server/src/main/java/com/inet/juchamsi/domain/chat/dto/request/ChatRoomRequest.java
+++ server/src/main/java/com/inet/juchamsi/domain/chat/dto/request/ChatRoomRequest.java
@@ -4,13 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class ChatRoomRequest {
     
     private String roomName;
-    
 }
