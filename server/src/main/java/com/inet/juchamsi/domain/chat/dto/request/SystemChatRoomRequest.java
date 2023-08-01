3576c906364595e1157bef5c3ef63d944df4097c
tngus2sh <ssuhyeon98@gmail.com>
2023-08-01 19:47:33 +0900

    feat: 시스템 대화방에서 사용하는 userId 담겨있는 request #S09P12C107-244

---
+++ server/src/main/java/com/inet/juchamsi/domain/chat/dto/request/SystemChatRoomRequest.java
@@ -0,0 +1,12 @@
+package com.inet.juchamsi.domain.chat.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SystemChatRoomRequest {
+
+    private String userId;
+
+}
