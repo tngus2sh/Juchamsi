f36b3a058d1d42e0d7a613e3a9f02d56e3f7e182
tngus2sh <ssuhyeon98@gmail.com>
2023-08-01 22:48:05 +0900

    feat: 메세지 저장 #S09P12C107-244

--- server/src/main/java/com/inet/juchamsi/domain/chat/application/ChatService.java
+++ server/src/main/java/com/inet/juchamsi/domain/chat/application/ChatService.java
@@ -1,5 +1,6 @@
 package com.inet.juchamsi.domain.chat.application;
 
+import com.inet.juchamsi.domain.chat.dto.request.ChatMessageRequest;
 import com.inet.juchamsi.domain.chat.dto.request.ChatRoomRequest;
 import com.inet.juchamsi.domain.chat.dto.request.SystemChatRoomRequest;
 import com.inet.juchamsi.domain.chat.dto.response.ChatRoomResponse;
@@ -13,9 +14,14 @@ public interface ChatService {
 
     public ChatRoomResponse showDetailChatRoom(String roomId);
 
+    // 메세지 저장
+    public void saveMessage(ChatMessageRequest request);
+
     /* 사용자끼리 대화방 */
+    // 대화방 생성
     public ChatRoomResponse createRoom(ChatRoomRequest request);
 
     /* 시스템 대화방 */
+    // 대화방 생성
     public ChatRoomResponse createSystemRoom(SystemChatRoomRequest request);
 }
