fd63f95452ff19b8e3a3ae09645f79a5a2c6397f
tngus2sh <ssuhyeon98@gmail.com>
2023-08-02 20:40:38 +0900

    refactor: right join fetch (지원 안함) -> left join fetch로 변경 #S09P12C107-248

--- server/src/main/java/com/inet/juchamsi/domain/chat/dao/MessageRepository.java
+++ server/src/main/java/com/inet/juchamsi/domain/chat/dao/MessageRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     // 해당 채팅방의 메세지 오래된 순으로 불러오기
-    @Query("select u.loginId, u.carNumber, m.content, m.createdDate from Message m right join fetch m.chatPeople cp right join fetch cp.chatRoom cr right join fetch cp.user u where cr.roomId=:roomId and cr.status=:status order by m.createdDate asc ")
+    @Query("select u.loginId, u.carNumber, m.content, m.createdDate from Message m left join fetch m.chatPeople cp left join fetch cp.chatRoom cr left join fetch cp.user u where cr.roomId=:roomId and cr.status=:status order by m.createdDate asc ")
     List<MessageResponse> findAllByChatRoom(@Param("roomId") String roomId, @Param("status") Status status);
 }
