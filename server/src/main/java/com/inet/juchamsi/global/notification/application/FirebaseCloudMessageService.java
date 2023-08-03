2d29e48c7544de1424d2ecbb5bc08aec4b8a7e8f
이진하 <ljh8190@naver.com>
2023-08-03 22:25:47 +0900

    refactor: #S09P12C107-253 주석 삭제

--- server/src/main/java/com/inet/juchamsi/global/notification/application/FirebaseCloudMessageService.java
+++ server/src/main/java/com/inet/juchamsi/global/notification/application/FirebaseCloudMessageService.java
@@ -55,49 +55,4 @@ public class FirebaseCloudMessageService {
             return "해당 유저가 존재하지 않습니다.";
         }
     }
-
-
-//    private final String APIP_URL = "";
-//    private final ObjectMapper objectMapper;
-//
-//    public void sendMessageTo(String targetToken, String title, String body, String path) throws JsonProcessingException {
-//        String message = makeMessage(targetToken, title, body, path);
-//
-//    }
-//
-//    private String makeMessage(String targetToken, String title, String body, String path) throws JsonProcessingException {
-//        FcmMessage fcmMessage = FcmMessage.builder()
-//                .message(FcmMessage.appMessage.builder()
-//                        .token(targetToken)
-//                        .notification(FcmMessage.Notification.builder()
-//                                .title(title)
-//                                .body(body)
-//                                .image(null)
-//                                .build()
-//                        )
-//                        .data(FcmMessage.FcmData.builder()
-//                                .path(path)
-//                                .build()
-//                        )
-//                        .build()
-//                ).validate_only(false).build();
-//
-//        log.info(objectMapper.writeValueAsString(fcmMessage));
-//        return objectMapper.writeValueAsString(fcmMessage);
-//    }
-//
-//    private String getAccessToken() throws Exception {
-//        // 2)
-//        String firebaseConfigPath = "key_path";
-//
-//        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
-//                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
-//
-//        // accessToken 생성
-//        googleCredentials.refreshIfExpired();
-//
-//        // GoogleCredential의 getAccessToken으로 토큰 받아온 뒤, getTokenValue로 최종적으로 받음
-//        // REST API로 FCM에 push 요청 보낼 때 Header에 설정하여 인증을 위해 사용
-//        return googleCredentials.getAccessToken().getTokenValue();
-//    }
 }
