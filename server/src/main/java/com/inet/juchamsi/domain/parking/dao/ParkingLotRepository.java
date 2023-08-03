d9405980fb77ee29218e6df0aaf5b38ee28bf6a3
소수현 <ssuhyeon98@gmail.com>
2023-08-03 17:04:14 +0900

    chore: 현재 주차여부 flag 삭제 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingLotRepository.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingLotRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
     @Query("select count(*) from ParkingLot p where p.villa=:villa")
     Long countByVilla(@Param("villa") Villa villa);
-
     List<ParkingLot> findByVilla_Id(Long villaId);
+    
+    // 입차시 입차 위치를 db에 업데이트
 }
