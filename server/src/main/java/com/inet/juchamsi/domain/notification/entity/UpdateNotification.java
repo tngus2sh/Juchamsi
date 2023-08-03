7e9025ae1548352ea2e8e0f624d091c130071fc8
이진하 <ljh8190@naver.com>
2023-08-04 00:42:02 +0900

    feat: #S09P12C107-252 jpa 기반 redis 객체 생성

---
+++ server/src/main/java/com/inet/juchamsi/domain/notification/entity/UpdateNotification.java
@@ -0,0 +1,31 @@
+package com.inet.juchamsi.domain.notification.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@RedisHash(value = "UpdateNotification")
+public class UpdateNotification {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String loginId;
+
+    @Indexed
+    private LocalDateTime outTime;
+
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    private Long timeToLive;
+}
