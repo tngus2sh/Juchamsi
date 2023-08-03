35c95bb5cab0b91ddd9905b8bd70ae89caefb6bf
tngus2sh <ssuhyeon98@gmail.com>
2023-08-03 23:23:06 +0900

    refactor: 사용자 찾기 오류 추가 #S09P12C107-248

--- server/src/main/java/com/inet/juchamsi/domain/chat/api/MessageApiController.java
+++ server/src/main/java/com/inet/juchamsi/domain/chat/api/MessageApiController.java
@@ -4,15 +4,18 @@ import com.inet.juchamsi.domain.chat.application.ChatService;
 import com.inet.juchamsi.domain.chat.dto.request.ChatMessageRequest;
 import com.inet.juchamsi.domain.chat.dto.request.SystemMessageRequest;
 import com.inet.juchamsi.global.api.ApiResult;
+import com.inet.juchamsi.global.error.NotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.inet.juchamsi.global.api.ApiResult.ERROR;
 import static com.inet.juchamsi.global.api.ApiResult.OK;
 
 @RestController
@@ -24,14 +27,17 @@ public class MessageApiController {
     private final ChatService chatService;
 
     @MessageMapping("/chat/message")
-//    public ApiResult<Void> enter(@DestinationVariable(value = "roomId") String roomId,  ChatMessageRequest request) { // @PathVariable이 아닌 @DestinationVariable로 roomId를 받아서 가져와야한다.
-    public ApiResult<Void> enter(ChatMessageRequest request) { // @PathVariable이 아닌 @DestinationVariable로 roomId를 받아서 가져와야한다.
+    public ApiResult<Void> enter(ChatMessageRequest request) {
         String roomId = request.getRoomId();
         if (ChatMessageRequest.MessageType.ENTER.equals(request.getType())) {
             request.setMessage(request.getSender() + "님이 입장하셨습니다.");
         }
         // 메세지 내용 저장
-        chatService.createChat(roomId, request);
+        try {
+            chatService.createChat(request);
+        } catch (NotFoundException e) {
+            ERROR("사용자를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
+        }
         
         // topic-1대다, queue-1대1
         sendingOperations.convertAndSend("/topic/chat/room/"+ roomId,request);
