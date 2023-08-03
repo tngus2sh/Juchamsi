63e09e7f11203b323505ae8b9088c563da446e7b
소수현 <ssuhyeon98@gmail.com>
2023-08-03 17:12:26 +0900

    feat: 입차 위치 정보 저장 TODO #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/application/impl/ParkingServiceImpl.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/application/impl/ParkingServiceImpl.java
@@ -1,11 +1,22 @@
 package com.inet.juchamsi.domain.parking.application.impl;
 
 import com.inet.juchamsi.domain.parking.application.ParkingService;
+import com.inet.juchamsi.domain.parking.dao.ParkingHistoryRepository;
+import com.inet.juchamsi.domain.parking.dto.request.EntranceRequest;
+import com.inet.juchamsi.domain.parking.entity.ParkingHistory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ParkingServiceImpl implements ParkingService {
+    
+    private final ParkingHistoryRepository parkingHistoryRepository;
 
+    // TODO: 입차 위치 정보 저장
+    @Override
+    public void createEntrance(EntranceRequest request) {
+//        ParkingHistory.createParkingHistory()
+//        parkingHistoryRepository.save();
+    }
 }
