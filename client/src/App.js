9f47f3e64340f320ea8e691b6ea0280bc1798b6a
임철성 <fpqaut@naver.com>
2023-08-05 14:58:03 +0900

    refactor: app.js 라우터 경로 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Reset as GlobalReset } from 'styled-reset';
 
 import LoginPage from './pages/web/login';
 import SignUpPage from './pages/web/signup';
 import WebMainPage from './pages/web/mainpage'
 import KioskMainPage from './pages/kiosk/main'
-import MobileMainPage from './pages/mobile/main'
 import KiosksavePage from './pages/kiosk/savepage'
 import KioskfindPage from './pages/kiosk/findpage'
 import KiosksavingPage from './pages/kiosk/savingpage'
@@ -27,22 +27,29 @@ import MobilePasswordUpdatePage from './pages/mobile/passwordchange'
 
 import './App.css';
 
-const isMobile = window.matchMedia('(max-width: 600px)').matches;
-const isKiosk = window.matchMedia('(max-width: 1000px)').matches;
-
 function App() {
+
   return (
     <div className="App">
         <GlobalReset/>
           <BrowserRouter>
             <Routes>
-            {isMobile ? (
-                <Route path="/" element={<MobileMainPage />} />
-              ) : isKiosk ? (
-                <Route path="/" element={<KioskMainPage />} />
-              ) : (
-                <Route path="/" element={<LoginPage />} />
-              )}
+            <Route
+            path="/"
+            element={
+              <Navigate
+                to={
+                  window.matchMedia('(max-width: 600px)').matches
+                    ? '/Mobile/Login'
+                    : window.matchMedia('(max-width: 1000px)').matches
+                    ? '/Kiosk/Main'
+                    : '/Web/Main'
+                }
+              />
+            }
+          />
+              <Route path="/Web/Main" element={<LoginPage />} />
+              <Route path="/Kiosk/Main" element={<KioskMainPage />} />
               <Route path="/Kiosk/savepage" element={<KiosksavePage />} />
               <Route path="/Kiosk/findpage" element={<KioskfindPage />} />
               <Route path="/Kiosk/savingpage" element={<KiosksavingPage />} />
