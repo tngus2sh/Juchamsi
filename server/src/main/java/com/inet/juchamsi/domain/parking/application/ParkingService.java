11272f812ed5024d73ee7df58b5e041fbbc0d2fc
tngus2sh <ssuhyeon98@gmail.com>
2023-08-05 17:19:28 +0900

    feat: 뒤 차주에게 현재 앞 차주의 출차시간에 대한 알람 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/application/ParkingService.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/application/ParkingService.java
@@ -1,6 +1,7 @@
 package com.inet.juchamsi.domain.parking.application;
 
 import com.inet.juchamsi.domain.parking.dto.request.EntranceExitRequest;
+import com.inet.juchamsi.domain.parking.dto.service.ExitTimeDto;
 
 import javax.transaction.Transactional;
 
@@ -9,6 +10,9 @@ public interface ParkingService {
 
     // 입차시 입차 정보 넣기
     void createEntrance(EntranceExitRequest request);
+
+    // 뒤 차주에게 현재 앞 차주의 출차시간에 대한 알람 주기
+    void notifyToBackNumber(ExitTimeDto exitTimeDto);
 
     // 출차시 출차 정보 넣기
     void createExit(EntranceExitRequest request);
