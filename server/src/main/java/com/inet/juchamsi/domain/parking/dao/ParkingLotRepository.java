48a4b308cfb5de41cd8335d85a50b78e9d04aba0
tngus2sh <ssuhyeon98@gmail.com>
2023-08-05 11:25:09 +0900

    refactor: 주차장 정보 자리 맥 주소로 찾기 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingLotRepository.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingLotRepository.java
@@ -18,4 +18,8 @@ public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
     // 자리 번호로 주차장 정보 가져오기
     @Query("select p from ParkingLot p where p.seatNumber=:seatNumber and p.active=:active")
     Optional<ParkingLot> findBySeatNumber(@Param("seatNumber") int seatNumber, @Param("active")Active active);
+
+    // 자리 맥 주소로 주차장 정보 가져오기
+    @Query("select p from ParkingLot p where p.seatMacAddress=:seatMacAddress and p.active=:active")
+    Optional<ParkingLot> findBySeatMacAddress(@Param("seatMacAddress") String seatMacAddress, @Param("active") Active active);
 }
