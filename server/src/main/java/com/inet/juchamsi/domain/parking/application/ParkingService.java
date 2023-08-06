c614a98b2a361ddf11ee79f244d10984c2d102b1
tngus2sh <ssuhyeon98@gmail.com>
2023-08-06 09:08:47 +0900

    feat: 출차시간 저장 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/application/ParkingService.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/application/ParkingService.java
@@ -1,6 +1,7 @@
 package com.inet.juchamsi.domain.parking.application;
 
 import com.inet.juchamsi.domain.parking.dto.request.EntranceExitRequest;
+import com.inet.juchamsi.domain.parking.dto.request.EntranceOutTimeRequest;
 import com.inet.juchamsi.domain.parking.dto.service.ExitTimeDto;
 
 import javax.transaction.Transactional;
@@ -10,6 +11,12 @@ public interface ParkingService {
 
     // 입차시 입차 정보 넣기
     void createEntrance(EntranceExitRequest request);
+
+    // 현재 차주의 출차시간 저장하기
+    void createOutTime(EntranceOutTimeRequest request);
+
+    // 현재 차주의 출차시간에 대한 알람 주기
+    void notifyToCarOwner(ExitTimeDto exitTimeDto);
 
     // 뒤 차주에게 현재 앞 차주의 출차시간에 대한 알람 주기
     void notifyToBackNumber(ExitTimeDto exitTimeDto);
