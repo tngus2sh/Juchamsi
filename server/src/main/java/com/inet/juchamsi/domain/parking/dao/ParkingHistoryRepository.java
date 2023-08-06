f566cc0f20381f8504eeed532c1dda85a200e601
tngus2sh <ssuhyeon98@gmail.com>
2023-08-06 14:28:37 +0900

    refactor: 출차 시간 타입 변경 Timestamp -> LocalDateTime #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingHistoryRepository.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dao/ParkingHistoryRepository.java
@@ -10,14 +10,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.swing.text.html.Option;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ParkingHistoryRepository extends JpaRepository<ParkingHistory, Long> {
     // 주차장 식별키와 자리로 현재 주차 되어있는지 확인
     @Query("select new com.inet.juchamsi.domain.parking.dto.service.BackUserOutTimeDto(u.loginId, ph.outTime)  from ParkingHistory ph left join ph.parkingLot pl left join ph.user u where pl.villa=:villa and pl.seatNumber=:seatNumber and ph.active=:active")
-    Optional<BackUserOutTimeDto> findByParkingHistoryAndActive(@Param("vila") Villa vill, @Param("seatNumber")int seatNumber, @Param("active") Active active);
+    Optional<BackUserOutTimeDto> findByParkingHistoryAndActive(@Param("villa") Villa vila, @Param("seatNumber")int seatNumber, @Param("active") Active active);
 
     // 주차장 맥 주소와 주차내역에서 현재 주차 되어 있다고 뜨는 목록 제일 최신 순으로 하나 가져오기
     @Query("select ph from ParkingHistory ph left join ph.parkingLot pl where pl.seatMacAddress=:seatMacAddress and ph.active=:active order by ph.createdDate desc")
@@ -35,5 +34,5 @@ public interface ParkingHistoryRepository extends JpaRepository<ParkingHistory, 
     // 주차장 히스토리 출차시간 업데이트
     @Modifying(clearAutomatically = true)
     @Query("update ParkingHistory ph set ph.outTime=:outTime where ph.id=:id")
-    Optional<Void> updateOutTime(@Param("outTime") Timestamp OutTime, @Param("id") Long id);
+    Optional<Void> updateOutTime(@Param("outTime") LocalDateTime OutTime, @Param("id") Long id);
 }
