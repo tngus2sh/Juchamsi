71d998a42a7e0384500d194c7589e3f4faff4770
jinha <ljh8190@naver.com>
2023-08-06 15:30:07 +0900

    refactor: 키오스크 페이지 디자인 수정

--- client/src/pages/kiosk/otherkeypage.js
+++ client/src/pages/kiosk/otherkeypage.js
@@ -1,36 +1,53 @@
-import Footer from './footer';
-import * as React from 'react';
-import Timer from './timer'
-import './otherkeypage.css'
-import Box from '@mui/material/Box';
-import { useNavigate } from 'react-router-dom';
+import Footer from "./footer";
+import * as React from "react";
+import Timer from "./timer";
+import "./otherkeypage.css";
+// import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 function OtherKeypage() {
-    let lockernum = 1
-    const navigate = useNavigate();
-    const handleOpenMainPage = () => {
-        // 메인 페이지로 이동
-        navigate('/');
-      };
-    return (
-        <div>
-            {/* 로고 */}
-            <img
-            className="logo"
-            src={process.env.PUBLIC_URL + '/img/kiosk/예비로고.png'}
-            alt={'title'}
-            ></img>
-            <Timer/>
-            <div className='otherkeytext1'>
-                <p>{lockernum}번 사물함의 키를 사용하신 이후 다시 보관하신다음</p>
-                <p>확인버튼을 눌러주시기 바랍니다.</p>
+  let lockernum = 1;
+  const navigate = useNavigate();
+  const handleOpenMainPage = () => {
+    // 메인 페이지로 이동
+    navigate("/");
+  };
+  return (
+    <div className="main">
+      {/* 로고 */}
+      <div className="logo-container">
+        <img className="kiosklogo" src={process.env.PUBLIC_URL + "/img/kiosk/logo.png"} alt={"title"}></img>
+      </div>
+
+      <div className="other-key-page-container">
+        <div className="other-key-page-flex-container">
+          <Timer />
+
+          <div className="other-key-page-info-container">
+            <div className="other-key-page-info">
+              <div className="other-key-page-text">
+                <div>
+                  <p className="other-key-page-info-text">
+                    {lockernum}번 사물함의 키를 사용하신 후,
+                    <br />
+                    다시 보관하시고
+                    <br />
+                    확인버튼을 눌러주세요.
+                  </p>
+                </div>
+
+                <div className="other-key-page-box" onClick={handleOpenMainPage}>
+                  <p className="other-key-page-info-button">확인</p>
+                </div>
+              </div>
             </div>
-            <Box component="span" className="otherkeybox1" onClick={handleOpenMainPage}>
-                <p className="otherkeytext2">확인</p>
-            </Box>
-            <Footer />
+          </div>
         </div>
-    )
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default OtherKeypage;
