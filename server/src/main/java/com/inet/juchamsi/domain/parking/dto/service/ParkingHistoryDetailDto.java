f4ada569ab95a308b79da8e790dd557b1c7720f6
소수현 <ssuhyeon98@gmail.com>
2023-08-07 11:15:09 +0900

    refactor: active 추가 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dto/service/ParkingHistoryDetailDto.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dto/service/ParkingHistoryDetailDto.java
@@ -1,5 +1,6 @@
 package com.inet.juchamsi.domain.parking.dto.service;
 
+import com.inet.juchamsi.global.common.Active;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,12 +13,14 @@ public class ParkingHistoryDetailDto {
     private LocalDateTime outTime;
     private int frontNumber;
     private int backNumber;
+    private Active active;
 
     @Builder
-    public ParkingHistoryDetailDto(String userId, LocalDateTime outTime, int frontNumber, int backNumber) {
+    public ParkingHistoryDetailDto(String userId, LocalDateTime outTime, int frontNumber, int backNumber, Active active) {
         this.userId = userId;
         this.outTime = outTime;
         this.frontNumber = frontNumber;
         this.backNumber = backNumber;
+        this.active = active;
     }
 }
