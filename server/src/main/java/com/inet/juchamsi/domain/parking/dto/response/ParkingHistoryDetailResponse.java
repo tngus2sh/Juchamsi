023d58b7ac489bd626c66fdda3f18afe9c803e61
소수현 <ssuhyeon98@gmail.com>
2023-08-07 11:15:24 +0900

    refactor: 앞차, 뒤차 사용자 아이디 추가 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dto/response/ParkingHistoryDetailResponse.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dto/response/ParkingHistoryDetailResponse.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-public class ParkingHistoryResponse {
+public class ParkingHistoryDetailResponse {
 
     private String userId; // 사용자 id
     private String outTime; // 출차시간
+    private String frontUserId; // 앞 차 사용자 아이디
+    private String backUserId; // 뒤 차 사용자 아이디
     private String frontParkingFlag; // 앞 차 주차 여부 FULL|EMPTY
     private String backParkingFlag; // 뒤 차 주차 여부 FULL|EMPTY
     private String frontOutTime; // 앞 차 출차 시간
@@ -16,9 +18,11 @@ public class ParkingHistoryResponse {
     private int backCarNumber; // 뒤 차 번호
 
     @Builder
-    public ParkingHistoryResponse(String userId, String outTime, String frontParkingFlag, String backParkingFlag, String frontOutTime, String backOutTime, int frontCarNumber, int backCarNumber) {
+    public ParkingHistoryDetailResponse(String userId, String outTime, String frontUserId, String backUserId, String frontParkingFlag, String backParkingFlag, String frontOutTime, String backOutTime, int frontCarNumber, int backCarNumber) {
         this.userId = userId;
         this.outTime = outTime;
+        this.frontUserId = frontUserId;
+        this.backUserId = backUserId;
         this.frontParkingFlag = frontParkingFlag;
         this.backParkingFlag = backParkingFlag;
         this.frontOutTime = frontOutTime;
