456aaa910d8c436c53e48a6afde1e3d157a5bfd2
kimsehoon <tpehd1009@gmail.com>
2023-08-09 04:07:29 +0100

    feat: #S09P12C1070-256 moveothermotor

--- IoT/JUCHAMSI/kioskModule/first.py
+++ IoT/JUCHAMSI/kioskModule/first.py
@@ -7,18 +7,21 @@ import RPi.GPIO as GPIO
 from time import sleep
 from flask_cors import CORS
 
-servoPin = 12
+servoPin1 = 12
+servoPin2 = 16
 SERVO_MAX_DUTY = 12
 SERVO_MIN_DUTY = 3
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPin, GPIO.OUT)
+GPIO.setup(servoPin1, GPIO.OUT)
+GPIO.setup(servoPin2, GPIO.OUT)
 
-servo = GPIO.PWM(servoPin, 50)
+servo1 = GPIO.PWM(servoPin1, 50)
+servo2 = GPIO.PWM(servoPin2, 50)
 
 ServerURL = "https://8ce6-121-178-98-21.ngrok-free.app"
-def operate_servo():
-    servo.start(0)
+def operate_servo1():
+    servo1.start(0)
     setServoPos(0)
     sleep(1)
     setServoPos(90)
@@ -29,7 +32,21 @@ def operate_servo():
     sleep(1)
     setServoPos(180)
     sleep(1)
-    servo.stop(0)
+    servo1.stop(0)
+
+def operate_servo2():
+    servo2.start(0)
+    setServoPos(0)
+    sleep(1)
+    setServoPos(90)
+    sleep(1)
+    setServoPos(50)
+    sleep(1)
+    setServoPos(120)
+    sleep(1)
+    setServoPos(180)
+    sleep(1)
+    servo2.stop(0)
 
 
 def setServoPos(degree):
@@ -37,7 +54,8 @@ def setServoPos(degree):
         degree = 180
     duty = SERVO_MIN_DUTY+(degree*(SERVO_MAX_DUTY-SERVO_MIN_DUTY)/180.0)
     print("Degree: {} to {}(Duty)".format(degree, duty))
-    servo.ChangeDutyCycle(duty)
+    servo1.ChangeDutyCycle(duty)
+    servo2.ChangeDutyCycle(duty)
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -55,17 +73,22 @@ def hello():
     print('hi')
     return 'Hello, World!'
 
-@app.route('/open', methods=['GET'])
+@app.route('/open', methods=['POST'])
 def open():
     # moduleName = request.args.get('key', default=None, type=str)
     # send ground mac add
-    operate_servo()
+    groundMacAddress = request.json['groundMacAddress']
     print(groundModule[1])
     return jsonify(success=True)
     
 @app.route('/close', methods=['POST'])
 def close():
-    groundMacAddress = request.form.get('groundMacAddress')
+    # groundMacAddress = request.form.get('groundMacAddress')
+    groundMacAddress = request.json['groundMacAddress']
+    if groundMacAddress == groundModule[0]:
+        operate_servo1()
+    elif groundMacAddress == groundModule[1]:
+        operate_servo2()
     print("Received data:", groundMacAddress)
     return jsonify(success=True, groundMacAddress=groundMacAddress)
 
