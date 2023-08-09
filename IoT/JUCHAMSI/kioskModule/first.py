3deac443152f670e7c914ddcde26a68860aa9f60
kimsehoon <tpehd1009@gmail.com>
2023-08-09 02:22:54 +0100

    feat: #S09P12C107-256 kioskalgotest

--- IoT/JUCHAMSI/kioskModule/first.py
+++ IoT/JUCHAMSI/kioskModule/first.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
+import subprocess
 import socket
 import requests
-
+import json
+ServerURL = "https://8ce6-121-178-98-21.ngrok-free.app"
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # We use a dummy IP here, as we just want to initiate the connection to retrieve the local IP
@@ -17,17 +19,24 @@ def hello():
     print('hi')
     return 'Hello, World!'
 
-@app.route('/open')
+@app.route('/open', methods=['GET'])
 def open():
-    moduleName = request.args.get('key', default=None, type=str)
-    return 'open'
+    # moduleName = request.args.get('key', default=None, type=str)
+    # send ground mac add
+    print(groundModule[1])
+    return jsonify(success=True)
     
-@app.route('/close')
+@app.route('/close', methods=['GET'])
 def close():
-    return 'close'
+    return jsonify(success=True)
+
 if __name__ == '__main__':
-    groundModule = ['b0:a7:32:db:c8:46','cc:db:a7:69:74:4a','cc:db:a7:69:19:7a','b0:a7:32:db:c3:52']
     app.debug = True
+    groundModule = ['b0:a7:32:db:c8:46','cc:db:a7:69:74:4a','cc:db:a7:69:19:7a','b0:a7:32:db:c3:52']
+    headers = {'Content-Type': 'application/json'}
     myIP = get_ip_address()
+    datas = {'kioskIP': myIP}
+    json_data = json.dumps(datas)
+    # response = requests.post(exit_url, data=json_data, headers=headers)
     print("IP: ", myIP)
     app.run(host="0.0.0.0", port=80)
