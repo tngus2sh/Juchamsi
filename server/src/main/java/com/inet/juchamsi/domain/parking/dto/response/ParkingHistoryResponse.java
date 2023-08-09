7fc424bbaa740c2f85214a51605ea3e832bda799
소수현 <ssuhyeon98@gmail.com>
2023-08-09 15:34:58 +0900

    refactor: 차 번호 추가 #S09P12C107-250

--- server/src/main/java/com/inet/juchamsi/domain/parking/dto/response/ParkingHistoryResponse.java
+++ server/src/main/java/com/inet/juchamsi/domain/parking/dto/response/ParkingHistoryResponse.java
@@ -9,13 +9,15 @@ public class ParkingHistoryResponse {
     
     private String userId;
     private int seatNumber;
+    private String carNumber;
     private String outTime;
     private String active; // ACTIVE : 주차가 됐다, DISABLED : 비어있는 상태
     
     @Builder
-    public ParkingHistoryResponse(String userId, int seatNumber, String outTime, String active) {
+    public ParkingHistoryResponse(String userId, int seatNumber, String carNumber, String outTime, String active) {
         this.userId = userId;
         this.seatNumber = seatNumber;
+        this.carNumber = carNumber;
         this.outTime = outTime;
         this.active = active;
     }
