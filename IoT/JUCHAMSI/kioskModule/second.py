0176029b313f64fedc1a9d093f14dd6e2945c152
kimsehoon <tpehd1009@gmail.com>
2023-08-10 09:52:47 +0100

    feat: #S09P12C107-256 teststart

--- IoT/JUCHAMSI/kioskModule/second.py
+++ IoT/JUCHAMSI/kioskModule/second.py
@@ -1,19 +1,12 @@
 from flask import Flask, jsonify, request
-from time import sleep
 import ble_803
 import sys
-import subprocess
 import socket
 import requests
 import bluetooth._bluetooth as bluez
-import json
 import uuid
-import numpy as np
-import serial
 import time
-import RPi.GPIO as GPIO
 import threading
-
 
 app = Flask(__name__)
 
@@ -21,20 +14,13 @@ def getMacAddress():
     mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
     return ':'.join(mac[i:i+2] for i in range(0,12,2))
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # We use a dummy IP here, as we just want to initiate the connection to retrieve the local IP
-    s.connect(("8.8.8.8", 80))
-    ip_address = s.getsockname()[0]
-    s.close()
-    return ip_address
-
 eventStartTimes = {}
+signalCounts = {}
 lastGetRequestTime = {}
 
 @app.route('/')
 def hello():
-    global eventStartTimes, lastGetRequestTime
+    global lastGetRequestTime
     macAddress = request.args.get('macAddress', None)
     if macAddress and macAddress in lastGetRequestTime:
         del lastGetRequestTime[macAddress]
@@ -42,18 +28,13 @@ def hello():
     return 'Hello, World!'
 
 def scan_bluetooth():
-    state = 0
-    cnt = 0
-    datas={}
     ground_module = ['b0:a7:32:db:c8:46', 'cc:db:a7:69:74:4a','cc:db:a7:69:19:7a', 'b0:a7:32:db:c3:52', '40:91:51:fc:fd:6a']
-    searched = [0, 0, 0, 0, 0]
     dev_id = 0
     try:
         sock = bluez.hci_open_dev(dev_id)
-        print "ble thread started"
-
+        print("ble thread started")
     except:
-        print "error accessing bluetooth device..."
+        print("error accessing bluetooth device...")
         sys.exit(1)
 
     ble_803.hci_le_set_scan_parameters(sock)
@@ -61,30 +42,41 @@ def scan_bluetooth():
 
     while True:
         returnedList = ble_803.parse_events(sock, 10)
-        print "----------"
+        print("----------")
         for beacon in returnedList:
             for comp_str in ground_module:
                 if(beacon[0:17]==comp_str):
-                    # ['mac', 'manuid', 'sonar', 'sonartime', 'tx', 'rssi', 'dis']
                     sendData = beacon.split(',')
-                    print sendData[0], sendData[2]
+                    print(sendData[0], sendData[2])
                     mac = sendData[0]
                     if int(sendData[2]) < 30:
+                        print(signalCounts)
                         if mac not in eventStartTimes:
                             eventStartTimes[mac] = time.time()
-                        elif time.time() -eventStartTimes[mac] > 100:
-                            if mac not in lastGetRequestTime or lastGetRequestTime:
-                                print("error")
+                            signalCounts[mac] = 1
+                        else:
+                            signalCounts[mac] += 1
+                            if signalCounts[mac] == 100:
+                                if mac not in lastGetRequestTime:
+                                    print("error")
+                                del eventStartTimes[mac]
+                                del signalCounts[mac]
+                                if mac in lastGetRequestTime:
+                                    del lastGetRequestTime[mac]
+                    else:
+                        print("clear")
+                        if mac in eventStartTimes:
                             del eventStartTimes[mac]
-                            if mac in lastGetRequestTime:
-                                del lastGetRequestTime
-    
+                        if mac in signalCounts:
+                            del signalCounts[mac]
+                        if mac in lastGetRequestTime:
+                            del lastGetRequestTime[mac]
+
+
 def runServer():
     app.run(debug=False, host="0.0.0.0", port=80)
-if __name__ == '__main__':
 
-    groundModule = ['b0:a7:32:db:c8:46','cc:db:a7:69:74:4a','cc:db:a7:69:19:7a','b0:a7:32:db:c3:52']
-    # response = requests.post(exit_url, data=json_data, headers=headers)
+if __name__ == '__main__':
     server_thread = threading.Thread(target=runServer)
     server_thread.start()
     scan_bluetooth()
