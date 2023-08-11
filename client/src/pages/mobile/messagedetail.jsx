44232836913201be154a3894395e5b42d6204486
이진하 <ljh8190@naver.com>
2023-08-11 16:44:33 +0900

    refactor: 채팅방 디자인 수정

--- client/src/pages/mobile/messagedetail.jsx
+++ client/src/pages/mobile/messagedetail.jsx
@@ -42,7 +42,6 @@ const Messagedetail = () => {
   const [messageStorage, setMessageStorage] = useState([]);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [messageLength, setMessageLength] = useState("");
   const wsRef = useRef(null);
   let prevDate = null;
 
@@ -63,6 +62,7 @@ const Messagedetail = () => {
   // }, []);
 
   useEffect(() => {
+    // setScreenSize();
     fetchMessage();
     connect();
   }, [roomId, senderId]);
@@ -90,16 +90,11 @@ const Messagedetail = () => {
       function (frame) {
         ws.subscribe("/topic/chat/room/" + roomId, function (message) {
           console.log("message 리스트??");
-          console.log(messageStorage.length);
-          console.log(messages.length);
+          // console.log(message);
           const recv = JSON.parse(message.body);
           recvMessage(recv);
         });
-        ws.send(
-          "/app/chat/message",
-          {},
-          JSON.stringify({ type: "ENTER", roomId: roomId, senderId: senderId })
-        );
+        ws.send("/app/chat/message", {}, JSON.stringify({ type: "ENTER", roomId: roomId, senderId: senderId }));
       },
       function (error) {
         console.log("Connection error:", error);
@@ -167,11 +162,7 @@ const Messagedetail = () => {
     if (!isSameDay(createDate, prevDate)) {
       prevDate = createDate;
 
-      return (
-        <React.Fragment>
-          <MessageSeparator content={format(createDate, "yyyy년 MM월 dd일")} />
-        </React.Fragment>
-      );
+      return <MessageSeparator content={format(createDate, "yyyy년 MM월 dd일")} />;
     }
   };
 
@@ -188,7 +179,132 @@ const Messagedetail = () => {
 
   return (
     <React.Fragment>
-      <Box
+      <div className="message-detail-main-container">
+        <div className="message-detail-main-flex-container">
+          <div className="message-detail-header-container">
+            <Box
+              sx={{
+                width: "100%",
+                height: "3.5rem",
+              }}
+            >
+              <Grid container sx={{ justifyContent: "center", height: "3.5rem", alignContent: "center" }}>
+                <ArrowBackIosRoundedIcon
+                  sx={{
+                    position: "fixed",
+                    top: "1.5rem",
+                    left: "1.2rem",
+                    width: "1.5rem",
+                    height: "1.5rem",
+                  }}
+                  onClick={handleBackToListClick}
+                />
+                <Typography sx={{ marginTop: "1rem", fontSize: "1.3rem", fontWeight: "bold" }}>{targetNickName}</Typography>
+              </Grid>
+            </Box>
+          </div>
+          <div className="message-detail-content-container" style={{ flex: "1 0 auto" }}>
+            <ChatContainer className="custom-chat-container">
+              <MessageList className="cs-message-list">
+                {messageStorage.length === 0 && messages.length === 0 ? (
+                  <MessageSeparator content="대화를 시작해주세요" />
+                ) : (
+                  messageStorage.map((message, index) => (
+                    <React.Fragment key={index}>
+                      <CurrentDate createdDate={message.createdDate} />
+                      <Message
+                        className="cs-message"
+                        model={{
+                          message: message.message,
+                          sender: message.loginId,
+                          direction: message.loginId === senderId ? "outgoing" : "incoming",
+                          position: "single",
+                        }}
+                        style={{ color: "white" }}
+                      >
+                        <Message.Footer
+                          sender={message.loginId === senderId ? "" : formatDateTime(message.createdDate)}
+                          sentTime={message.loginId === senderId ? formatDateTime(message.createdDate) : ""}
+                        />
+                      </Message>
+                    </React.Fragment>
+                  ))
+                )}
+
+                {messages.map((message, index) =>
+                  message.type === "ENTER" ? null : ( // <MessageSeparator key={index} content={message.message} as="h2" />
+                    <React.Fragment>
+                      <CurrentDate createdDate={new Date()} />
+                      <Message
+                        key={index}
+                        model={{
+                          message: message.message,
+                          sender: message.senderId,
+                          direction: message.senderId === senderId ? "outgoing" : "incoming",
+                          position: "single",
+                        }}
+                      >
+                        <Message.Footer
+                          sender={message.senderId === senderId ? "" : new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "")}
+                          sentTime={message.senderId === senderId ? new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "") : ""}
+                        />
+                      </Message>
+                    </React.Fragment>
+                  )
+                )}
+              </MessageList>
+            </ChatContainer>
+          </div>
+          <div className="message-detail-footer-container">
+            <Grid
+              container
+              sx={{
+                boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.2)",
+                width: "100%",
+                height: "4.9rem",
+                // position: "fixed",
+                // bottom: 0,
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+                backgroundColor: "white",
+              }}
+            >
+              <Grid item sx={{ flex: 1, p: ".3rem" }}>
+                <input
+                  style={{
+                    width: "95%",
+                    padding: ".31rem",
+                    paddingLeft: ".8rem",
+                    height: "2rem",
+                    borderRadius: "200px",
+                    border: "none",
+                    backgroundColor: "#f0f0f0",
+                    outline: "none",
+                  }}
+                  value={message}
+                  onChange={handleMessageChange}
+                  onKeyDown={handleKeyDown}
+                />
+              </Grid>
+              <Grid item sx={{ p: ".5rem" }}>
+                <Fab color="primary" sx={{ width: "2.8rem", height: "2.8rem" }} aria-label="add">
+                  <SendIcon
+                    onClick={() => {
+                      if (message.trim() !== "") {
+                        sendMessage();
+                        setMessage("");
+                      }
+                    }}
+                  />
+                </Fab>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </div>
+
+      {/* <Box
         sx={{
           width: "100%",
           height: "3.3rem",
@@ -207,9 +323,7 @@ const Messagedetail = () => {
             }}
             onClick={handleBackToListClick}
           />
-          <Typography sx={{ marginTop: "1rem", fontSize: "1.3rem", fontWeight: "bold" }}>
-            {targetNickName}
-          </Typography>
+          <Typography sx={{ marginTop: "1rem", fontSize: "1.3rem", fontWeight: "bold" }}>{targetNickName}</Typography>
         </Grid>
       </Box>
       <div
@@ -238,12 +352,8 @@ const Messagedetail = () => {
                     style={{ color: "white" }}
                   >
                     <Message.Footer
-                      sender={
-                        message.loginId === senderId ? "" : formatDateTime(message.createdDate)
-                      }
-                      sentTime={
-                        message.loginId === senderId ? formatDateTime(message.createdDate) : ""
-                      }
+                      sender={message.loginId === senderId ? "" : formatDateTime(message.createdDate)}
+                      sentTime={message.loginId === senderId ? formatDateTime(message.createdDate) : ""}
                     />
                   </Message>
                 </React.Fragment>
@@ -252,7 +362,7 @@ const Messagedetail = () => {
 
             {messages.map((message, index) =>
               message.type === "ENTER" ? null : ( // <MessageSeparator key={index} content={message.message} as="h2" />
-                <React.Fragment key={index}>
+                <React.Fragment>
                   <CurrentDate createdDate={new Date()} />
                   <Message
                     key={index}
@@ -264,16 +374,8 @@ const Messagedetail = () => {
                     }}
                   >
                     <Message.Footer
-                      sender={
-                        message.senderId === senderId
-                          ? ""
-                          : new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "")
-                      }
-                      sentTime={
-                        message.senderId === senderId
-                          ? new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "")
-                          : ""
-                      }
+                      sender={message.senderId === senderId ? "" : new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "")}
+                      sentTime={message.senderId === senderId ? new Date().toLocaleTimeString("ko-KR").replace(/:\d+$/, "") : ""}
                     />
                   </Message>
                 </React.Fragment>
@@ -325,7 +427,7 @@ const Messagedetail = () => {
             />
           </Fab>
         </Grid>
-      </Grid>
+      </Grid> */}
     </React.Fragment>
   );
 };
