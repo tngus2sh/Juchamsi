6e6f8f93c986dd7c9afa1ec40acbf26f38c79dd8
KSH <tpehd1009@gmail.com>
2023-08-13 23:28:27 +0900

    createv4

--- IoT/carModule/totalcodev4.py
+++ IoT/carModule/totalcodev4.py
@@ -1,12 +1,9 @@
-
-import ble_803
+import advertiseMac
 import sys
 import requests
 import bluetooth._bluetooth as bluez
-import math
 import uuid
 import requests
-import numpy as np
 import json
 import serial
 import time
@@ -21,7 +18,6 @@ def cal_dis(rssi, tx):
 
 def scan_bluetooth():
     sonarCount = {}
-    state = 0
     cnt = 0
     repeat = 0
     # parking_url="http://i9c107.p.ssafy.io:8080/parking/entrance"
@@ -32,7 +28,6 @@ def scan_bluetooth():
     datas={}
     ground_module = ['b0:a7:32:db:c8:46', 'cc:db:a7:69:74:4a','cc:db:a7:69:19:7a', 'b0:a7:32:db:c3:52']
     searched = [0, 0, 0, 0]
-    park_state = []
     lastSonarValues = {}
     
 
@@ -45,12 +40,12 @@ def scan_bluetooth():
         print "error accessing bluetooth device..."
         sys.exit(1)
 
-    ble_803.hci_le_set_scan_parameters(sock)
-    ble_803.hci_enable_le_scan(sock)
+    advertiseMac.hci_le_set_scan_parameters(sock)
+    advertiseMac.hci_enable_le_scan(sock)
 
     while True:
         repeat += 1
-        returnedList = ble_803.parse_events(sock, 10)
+        returnedList = advertiseMac.parse_events(sock, 10)
         print "----------"
         if repeat == 20:
             print('not park')
@@ -63,7 +58,7 @@ def scan_bluetooth():
                 datas = {'macAddress': getMacAddress()}
                 json_data = json.dumps(datas)
                 try:
-                    response = requests.post(exit_url, data=json_data, headers=headers)
+                    requests.post(exit_url, data=json_data, headers=headers)
                     print("Successfully sent POST request to exit_url.")
                 except Exception as e:
                     print("Failed to send request to exit_url. Error: {}".format(e))
@@ -77,8 +72,8 @@ def scan_bluetooth():
                     # ['mac', 'manuid', 'sonar', 'sonartime', 'tx', 'rssi', 'dis']
                     sendData = beacon.split(',')
                     print(sendData)
-
                     nowSonar = int(sendData[2])
+                    print(nowSonar)
                     if nowSonar not in sonarCount:
                         sonarCount[nowSonar] = 0
                     sonarCount[nowSonar] += 1
@@ -99,7 +94,6 @@ def scan_bluetooth():
                         # send Frequency
                         if cnt == 100:
                             rpiMac = getMacAddress()
-                            mostFreqSonar = max(sonarCount, key = sonarCount.get)
                             mostFrequentMacIndex = searched.index(max(searched))
                             mostFrequentMac = ground_module[mostFrequentMacIndex]
                             mostRecentSonar = lastSonarValues[mostFrequentMac]
@@ -117,25 +111,15 @@ def scan_bluetooth():
                                         nowParking[mostFrequentMacIndex] = 1
                                         print(nowParking[mostFrequentMacIndex])
                                         print('car_in')
-                                        response = requests.post(parking_url, data=json_data, headers=headers, timeout=1)
+                                        requests.post(parking_url, data=json_data, headers=headers, timeout=1)
                                         try:
-                                            response = requests.post(soundModuleUrl, data=json_data, headers=headers, timeout=1)
+                                            requests.post(soundModuleUrl, data=json_data, headers=headers, timeout=1)
                                         except Exception as e:
                                             print("Failed to send request to soundModuleUrl. Error: {}".format(e))
                                     except Exception as e:
                                         print("Failed BackServer")
                                         nowParking[mostFrequentMacIndex] = 0
-                                    
-                            else:
-                                if nowParking[mostFrequentMacIndex] == 1:
-                                    headers = {'Content-Type': 'application/json'}
-                                    datas = {'macAddress': rpiMac}
-                                    nowParking[mostFrequentMacIndex] = 0
-                                    print('car_out')
-                                    json_data = json.dumps(datas)
-                                    response = requests.post(exit_url, data=json_data, headers=headers)
-                            print searched.index(max(searched)), mostRecentSonar
-                            return searched.index(max(searched))
+
 
 def safe_readline(ser):
     try:
